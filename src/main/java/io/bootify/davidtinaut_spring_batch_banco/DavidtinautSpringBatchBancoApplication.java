a01cd047165259d89adcf38d3a0422407c37e213
davidtinaut <davidtinaut20@gmail.com>
2023-12-22 11:45:06 +0100

    no se que pasa

--- src/main/java/io/bootify/davidtinaut_spring_batch_banco/DavidtinautSpringBatchBancoApplication.java
+++ src/main/java/io/bootify/davidtinaut_spring_batch_banco/DavidtinautSpringBatchBancoApplication.java
@@ -3,11 +3,10 @@ package io.bootify.davidtinaut_spring_batch_banco;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-public class DavidtinautSpringBatchBancoApplication {
+public class DavidtinautSpringBatchBancoApplication{
 
-    public static void main(final String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(DavidtinautSpringBatchBancoApplication.class, args);
     }
 
--- src/main/java/io/bootify/davidtinaut_spring_batch_banco/repos/TransaccionesRepository.java
+++
@@ -1,8 +0,0 @@
-package io.bootify.davidtinaut_spring_batch_banco.repos;
-
-import io.bootify.davidtinaut_spring_batch_banco.model.TransaccionesDTO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransaccionesRepository extends JpaRepository<TransaccionesDTO,String> {
-
-}
